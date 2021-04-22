1509cef2bf4068962cbcc4433c23fd6df6468d8b
LdMe <dlafuente003@gmail.com>
2021-04-22 16:32:48 +0200

    fps and speed info added to file name

--- src/video/videoWriter.py
+++ src/video/videoWriter.py
@@ -16,7 +16,9 @@ class TimeLapseWriter:
 		self.timeLapseTimer = TimeLapseTimer(self.specs)
 		if not os.path.exists('static/media/'):
 			os.makedirs('static/media/')
-		self.filename = "static/media/"+self.timeLapseTimer.getFormattedStartTime()+'.webm'
+		fps = self.getSpecs()["fps"]
+		self.speed = self.getSpecs()["speed"]
+		self.filename = "static/media/"+self.timeLapseTimer.getFormattedStartTime()+"x"+speed+"-"+fps+'fps.webm'
 		self.videoWriter = cv2.VideoWriter(self.filename,self.fourcc, self.specs.fps, self.frameSize)
 
 	def record(self):
