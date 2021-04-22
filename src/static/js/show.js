f5adf450dfbac8caa9059c5fe5de5d5b556d06a9
LdMe <dlafuente003@gmail.com>
2021-04-23 00:26:22 +0200

    videos showing by steps

--- src/static/js/show.js
+++ src/static/js/show.js
@@ -1,6 +1,23 @@
 $(function(){
 	$("#recordedButton").addClass("btn-dark")
 	$("#homeButton").removeClass("btn-dark")
-	
-
+	let elementsQuantity =  $('.recordedVideo').length
+	let showQuantity = 10
+	function showBySteps() {
+		let counter = 0
+		$(".recordedVideo").each(() => {
+			if(counter <  showQuantity){
+				$(this).hide()
+			}
+			else {
+				$(this).show()
+			}
+			counter++
+		})
+	}
+	$("#showMore").click(()=>{
+		showQuantity += 10
+		showBySteps()
+	})
+	showBySteps()
 })
