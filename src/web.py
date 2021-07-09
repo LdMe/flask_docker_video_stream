8c28fb13666491b3aee1fd7d5d58b7833a988753
Danel <dlafuente003@gmail.com>
2021-07-09 12:06:08 +0200

    removed unnecesary libraries

--- src/web.py
+++ src/web.py
@@ -4,7 +4,6 @@ from video.videoServer import WebServer
 from threading import Thread
 import time
 from arduino.server import SocketServer
-from flask_talisman import Talisman
 
 app = Flask(__name__,template_folder="views",static_folder='static')
 webServer= WebServer()	
